fa992210b9f531aafc7abae078735480ae5aee3b
Ryan Jones <ryandrewjones@gmail.com>
2024-01-07 17:35:56 -0700

    fixing html theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,6 @@ release = '3.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autosectionlabel',
-              'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,7 +47,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'master'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
